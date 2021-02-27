6f87e740ca0b9f818338301117bf1951ebf583dd
Andy Craze <accraze@gmail.com>
2021-02-27 09:52:53 -0800

    add flycheck error threshold

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-checker-error-threshold 1000)
  '(org-agenda-custom-commands
    '((" " "Agenda"
       ((agenda ""
@@ -42,3 +43,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'customize-variable 'disabled nil)
